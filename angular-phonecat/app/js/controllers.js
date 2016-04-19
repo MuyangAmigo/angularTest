456c199ab2d9c8560ea5a7c64f7eefdadc585065
Junjie Li <jj.muyang@gmail.com>
2016-04-19 18:15:29 -0500

    serve the code
    have to serve the code when you try http service and its on local.

--- angular-phonecat/app/js/controllers.js
+++ angular-phonecat/app/js/controllers.js
@@ -3,32 +3,32 @@
 /* Controllers */
 
 var phonecatControllers = angular.module('phonecatControllers', []);
+var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl',
-  function ($scope) {
-    $scope.phones = [
-      {'name': 'Junjie Li',
-       'snippet': 'hhhhhhh'},
-      {'name': 'Shabi',
-       'snippet': 'ooooooo'},
-      {'name': 'yoyoyoyo',
-       'snippet': 'ggggggg'}
-    ];
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
+  $http.get('phones/phones.json').success(function(data) {
+  $scope.phones = data;
   });
 
-phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-  function($scope, Phone) {
-    $scope.phones = Phone.query();
-    $scope.orderProp = 'age';
-  }]);
+  $scope.orderProp = 'age';
+});
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
-
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    };
-  }]);
+// phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+//   function($scope, Phone) {
+//     $scope.phones = Phone.query();
+//     $scope.orderProp = 'age';
+//   }]);
+//
+// phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+//   function($scope, $routeParams, Phone) {
+//     $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+//       $scope.mainImageUrl = phone.images[0];
+//     });
+//
+//     $scope.setImage = function(imageUrl) {
+//       $scope.mainImageUrl = imageUrl;
+//     };
+//   }]);
